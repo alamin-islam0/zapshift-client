fa3cfc773a839dd947c2d5ce1386e7f96ccc0298
Alamin Islam <alamin648890@gmail.com>
2025-11-18 23:49:58 +0600

    theme setup done

--- src/pages/shared/header/Header.jsx
+++ src/pages/shared/header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl"><img src={Logo} alt="" /></a>
+        <a className=" btn-ghost text-xl"><img src={Logo} alt="logo" /></a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
@@ -43,7 +43,7 @@ const Header = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn my-btn">Button</a>
       </div>
     </div>
   );
