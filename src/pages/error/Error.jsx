2812ceffe1f97d73a3da1259e4c6ae697b493223
Alamin Islam <alamin648890@gmail.com>
2025-11-25 01:20:57 +0600

    Path error page added

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router";
+import ErrorIcon from "../../assets/error.svg";
 
 const Error = () => {
-    return (
-        <div>
-            
+  const navigate = useNavigate();
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white py-12 px-6">
+      <div className="text-center">
+        {/* Error Icon */}
+        <div className="mb-8 flex justify-center">
+          <img
+            src={ErrorIcon}
+            alt="Error 404"
+            className="w-48 h-48 lg:w-64 lg:h-64"
+          />
         </div>
-    );
+
+        {/* Go Home Button */}
+        <button
+          onClick={() => navigate("/")}
+          className="bg-[#caeb66] hover:bg-[#b8d955] text-[#03373d] font-semibold rounded-full px-8 py-3 text-base lg:text-lg transition-all duration-300 shadow-sm hover:shadow-md"
+        >
+          Go Home
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,25 +3,30 @@ import RootLayouts from "../layouts/RootLayouts";
 import Home from "../pages/home/Home";
 import Coverage from "../pages/coverage/Coverage";
 import About from "../pages/about/About";
+import Error from "../pages/error/Error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayouts,
     children: [
-        {
-            index: true,
-            Component: Home
-        },
-        {
-            path: 'coverage',
-            Component: Coverage,
-            loader: () => fetch('/serviceCenter.json').then(res => res.json())
-        },
-        {
-            path: 'about-us',
-            Component: About
-        }
-    ]
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "coverage",
+        Component: Coverage,
+        loader: () => fetch("/serviceCenter.json").then((res) => res.json()),
+      },
+      {
+        path: "about-us",
+        Component: About,
+      },
+    ],
+  },
+  {
+    path: "*",
+    Component: Error,
   },
 ]);
